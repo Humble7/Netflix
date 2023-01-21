c027839d4dee3b7d9c450ff63ff1f947f40cca45
Chen Zhen <chenzhennba@gmail.com>
2023-01-21 20:43:43 +0800

    build base ui framework.

---
+++ Netflix Clone/Controlllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by mozat on 21/1/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controlllers/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by mozat on 21/1/23.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
